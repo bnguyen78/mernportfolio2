24c0d91893c93e5beab5945d27abab312ed4a68f
Bryant Nguyen <bryant_72002@yahoo.com>
2020-03-11 21:03:57 -0700

    added applisten port

--- server.js
+++ server.js
@@ -8,3 +8,7 @@ const app = express()
 app.use(express.static(join(__dirname, 'client', 'build')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
