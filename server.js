ed89e3b2ac4a58e09097296e9d43b4ded5f08c30
Bryant Nguyen <bryant_72002@yahoo.com>
2020-03-11 21:18:06 -0700

    removed previous changes

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ app.use(express.static(join(__dirname, 'client', 'build')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
-
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
