8457c3f3c5cc7a9dbef4da730eb24fc239cf3574
Bryant Nguyen <bryant_72002@yahoo.com>
2020-03-11 21:24:01 -0700

    added portfolio and contact to try to fix heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { join } = require('path')
 
 const app = express()
 
-app.use(express.static(join(__dirname, 'client', 'build')))
+app.use(express.static(join(__dirname, 'client', 'build', '/portfolio', '/contact')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
