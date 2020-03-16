2660ff0a5caa2d096c21f99600e64bad50947ff4
Bryant Nguyen <bryant_72002@yahoo.com>
2020-03-16 10:50:32 -0700

    updated server

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use(express.static(join(__dirname, 'client', 'build')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3001, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
