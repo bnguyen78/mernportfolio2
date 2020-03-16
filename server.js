9e86948289382ce7775ed88f265eb24a64c02bdd
Bryant Nguyen <bryant_72002@yahoo.com>
2020-03-16 13:37:11 -0700

    added app.get to see if it works on heroku

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.use(express.static(join(__dirname, 'client', 'build')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.get('/', function (req, res) {
+  app.get('/*', function (req, res) {
+     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+   });
+
 app.listen(process.env.PORT || 3001, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
